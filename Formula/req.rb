84cbf88b514dae996e12448552c60e26acf30f5c
khoa <khoa210611@icloud.com>
2026-03-21 22:02:42 +0700

    changed formula

--- Formula/req.rb
+++ Formula/req.rb
@@ -20,10 +20,18 @@ class Req < Formula
       url
   "https://github.com/kevkoa2106/req/releases/download/v0.1.2/req-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "LINUX_X86_SHA256_HERE"
+    else
+      url "https://github.com/kevkoa2106/req/archive/refs/tags/v0.1.2.tar.gz"
+      sha256 "0ce6f6001690f451a0578e0615eeb1af0a8649dd54853d0115263dbeddb5169b"
+      depends_on "rust" => :build
     end
 
     def install
-      bin.install "req"
+      if build.head? || File.exist?("Cargo.toml")
+        system "cargo", "install", *std_cargo_args
+      else
+        bin.install "req"
+      end
     end
 
     test do
