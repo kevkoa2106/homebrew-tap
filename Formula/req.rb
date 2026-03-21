94ea16db5e7e07a605caf84020a8ff9de07c42a3
khoa <khoa210611@icloud.com>
2026-03-21 21:15:06 +0700

    changed formula

--- Formula/req.rb
+++ Formula/req.rb
@@ -3,8 +3,24 @@ class Req < Formula
     homepage "https://github.com/kevkoa2106/req"
     version "0.1.2"
     license "GPL-3.0-only"
-    url "https://github.com/kevkoa2106/req/releases/download/v0.1.2/req-aarch64-apple-darwin.tar.gz"
-    sha256 "93e7d84bd7783777ff2e408e0f74b1ebf5a9534c437ed1150d1eba80dff424db"
+
+    if OS.mac? && Hardware::CPU.arm?
+      url
+  "https://github.com/kevkoa2106/req/releases/download/v0.1.2/req-aarch64-apple-darwin.tar.gz"
+      sha256 "93e7d84bd7783777ff2e408e0f74b1ebf5a9534c437ed1150d1eba80dff424db"
+    elsif OS.mac? && Hardware::CPU.intel?
+      url
+  "https://github.com/kevkoa2106/req/releases/download/v0.1.2/req-x86_64-apple-darwin.tar.gz"
+      sha256 "INTEL_MAC_SHA256_HERE"
+    elsif OS.linux? && Hardware::CPU.arm?
+      url
+  "https://github.com/kevkoa2106/req/releases/download/v0.1.2/req-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "LINUX_ARM_SHA256_HERE"
+    elsif OS.linux? && Hardware::CPU.intel?
+      url
+  "https://github.com/kevkoa2106/req/releases/download/v0.1.2/req-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "LINUX_X86_SHA256_HERE"
+    end
 
     def install
       bin.install "req"
