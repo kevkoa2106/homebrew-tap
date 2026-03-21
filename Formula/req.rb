dba1a6c310a0515a55a262d1acf1b43fd2ed790c
khoa <khoa210611@icloud.com>
2026-03-21 21:02:39 +0700

    changed formula

--- Formula/req.rb
+++ Formula/req.rb
@@ -1,17 +1,22 @@
 class Req < Formula
-    desc "Lightweight HTTP client for .rest files"
-    homepage "https://github.com/kevkoa2106/req"
-    url "https://github.com/kevkoa2106/req/archive/refs/tags/v0.1.2.tar.gz"
-    sha256 "0ce6f6001690f451a0578e0615eeb1af0a8649dd54853d0115263dbeddb5169b"
-    license "GPL-3.0-only"
-    
-    depends_on "rust" => :build
+  desc "Lightweight HTTP client for .rest files"
+  homepage "https://github.com/kevkoa2106/req"
+  version "0.1.2"
+  license "GPL-3.0-only"
 
-    def install
-      system "cargo", "install", *std_cargo_args
+  on_macos do
+    if Hardware::CPU.arm?
+      url
+"https://github.com/kevkoa2106/req/releases/download/v0.1.2/req-aarch64-apple-darwin.tar.gz"
+      sha256 "93e7d84bd7783777ff2e408e0f74b1ebf5a9534c437ed1150d1eba80dff424db"
     end
+  end
 
-    test do
-      assert_match "req", shell_output("#{bin}/req --version")
-    end
+  def install
+    bin.install "req"
+  end
+
+  test do
+    assert_match "req", shell_output("#{bin}/req --version")
+  end
 end
