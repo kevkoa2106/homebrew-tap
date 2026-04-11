14e0df5976b82e1e0018006df851162ac1d0ec69
khoa <khoa210611@icloud.com>
2026-04-11 10:40:48 +0700

    edited formula

--- Formula/mutti.rb
+++ Formula/mutti.rb
@@ -1,24 +1,17 @@
 class Mutti < Formula
   desc "Terminal music player"
   homepage "https://github.com/kevkoa2106/mutti"
-  version "0.1.0"
+  version "0.2.0"
   license "GPL-3.0-only"
 
   on_macos do
-    on_arm do
-      url "https://github.com/kevkoa2106/mutti/releases/download/v0.1.0/mutti-aarch64-apple-darwin.tar.gz"
-      sha256 "6047ad7de398968ab6075f438f8c9b3f79e8adcf2620b1eabcca1857d24cef7d"
-    end
-
-    on_intel do
-      url "https://github.com/kevkoa2106/mutti/releases/download/v0.1.0/mutti-x86_64-apple-darwin.tar.gz"
-      sha256 "c0152206276a283d08039994a4a436d1fc9d3ab7b9375ba31e538ff4dbe810d5"
-    end
+    url "https://github.com/kevkoa2106/mutti/releases/download/v0.2.0/mutti-arm64-apple-darwin.tar.gz"
+    sha256 "9c72d43a46ad5ae2d16fb58ada0ea6cdf5a5ab2763be946e4a0202e3eefe9b74"
   end
 
   on_linux do
-    url "https://github.com/kevkoa2106/mutti/releases/download/v0.1.0/mutti-x86_64-linux.tar.gz"
-    sha256 "a680f9fbcc219a47f3316e5734976bd90f6bcd748ef8d0b6f2f958ad90f2ef9c"
+    url "https://github.com/kevkoa2106/mutti/releases/download/v0.2.0/mutti-x86_64-linux.tar.gz"
+    sha256 "ad8e24a8cdc13711728b050cf8f83cc0826828b1756809a871b633417033c2b8"
   end
 
   def install
