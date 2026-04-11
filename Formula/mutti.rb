97f50d9deb90ea8be537174bee3efc3ddafbbfac
khoa <khoa210611@icloud.com>
2026-04-11 12:51:01 +0700

    edited formula

--- Formula/mutti.rb
+++ Formula/mutti.rb
@@ -1,17 +1,17 @@
 class Mutti < Formula
   desc "Terminal music player"
   homepage "https://github.com/kevkoa2106/mutti"
-  version "0.2.0"
+  version "0.3.0"
   license "GPL-3.0-only"
 
   on_macos do
-    url "https://github.com/kevkoa2106/mutti/releases/download/v0.2.0/mutti-arm64-apple-darwin.tar.gz"
-    sha256 "9c72d43a46ad5ae2d16fb58ada0ea6cdf5a5ab2763be946e4a0202e3eefe9b74"
+    url "https://github.com/kevkoa2106/mutti/releases/download/v0.3.0/mutti-arm64-apple-darwin.tar.gz"
+    sha256 "527c202bce891b0f4388122b44c59dd5b6e587af27a63e0e2803fc4058339d15"
   end
 
   on_linux do
-    url "https://github.com/kevkoa2106/mutti/releases/download/v0.2.0/mutti-x86_64-linux.tar.gz"
-    sha256 "ad8e24a8cdc13711728b050cf8f83cc0826828b1756809a871b633417033c2b8"
+    url "https://github.com/kevkoa2106/mutti/releases/download/v0.3.0/mutti-x86_64-linux.tar.gz"
+    sha256 "c375a7eef2637a6f773fe5a758046f8d4c0a48633e8c5541185eaad08046fb0b"
   end
 
   def install
